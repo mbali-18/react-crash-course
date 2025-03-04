6fab6267d0ee323637c85876d7bbaa65540bd663
mbali-18 <faithfulservent18@gmail.com>
2025-03-04 12:32:44 +0200

    added post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,11 @@
+const names = ["Mbali", "Mbuso"];
+
 function Post() {
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
   return (
     <div>
-      <p>Mbali</p>
+      <p>{chosenName}</p>
       <p>React.js is awesome!</p>
     </div>
   );
