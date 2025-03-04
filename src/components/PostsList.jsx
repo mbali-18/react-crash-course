2d5c7947ba4a728d070ce5f1484c860a5bc38922
mbali-18 <faithfulservent18@gmail.com>
2025-03-04 17:00:18 +0200

    State Management

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,16 @@
 import Post from "./Post";
+import NewPost from "./NewPost";
 import classes from "./PostsList.module.css";
 
 function PostList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Mbali" body="React.js is awesome!!" />
-      <Post author="Mbuso" body="Check out the full course" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Mbali" body="React.js is awesome!!" />
+        <Post author="Mbuso" body="Check out the full course" />
+      </ul>
+    </>
   );
 }
 
