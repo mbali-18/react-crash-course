2aeb38f09cd5b3ddce799aacdd5502632851e24f
mbali-18 <faithfulservent18@gmail.com>
2025-03-05 18:19:59 +0200

    State & Conditional Content

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,8 +6,13 @@ import Modal from "./Modal";
 import classes from "./PostsList.module.css";
 
 function PostList() {
+  const [modalVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
 
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
@@ -17,14 +22,20 @@ function PostList() {
     setEnteredAuthor(event.target.value);
   }
 
+  let modalContent;
+
+  if (setModalIsVisible) {
+    <Modal onClose={hideModalHandler}>
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
+    </Modal>;
+  }
+
   return (
     <>
-      <Modal>
-        <NewPost
-          onBodyChange={bodyChangeHandler}
-          onAuthorChange={authorChangeHandler}
-        />
-      </Modal>
+      {modalContent}
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Mbuso" body="Check out the full course" />
