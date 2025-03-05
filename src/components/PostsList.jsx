aaa909eea07204af09c9feafb6ddd82cddd4914c
mbali-18 <faithfulservent18@gmail.com>
2025-03-05 12:54:11 +0200

    Lifting State Up

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
+
 import Post from "./Post";
 import NewPost from "./NewPost";
 import classes from "./PostsList.module.css";
 
 function PostList() {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
       <ul className={classes.posts}>
-        <Post author="Mbali" body="React.js is awesome!!" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Mbuso" body="Check out the full course" />
       </ul>
     </>
