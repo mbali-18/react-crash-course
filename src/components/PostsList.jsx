59e2af8f21693d617d97b96633837b33cc1b0628
mbali-18 <faithfulservent18@gmail.com>
2025-03-05 17:03:52 +0200

    The Special "children" Prop

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import Post from "./Post";
 import NewPost from "./NewPost";
+import Modal from "./Modal";
 import classes from "./PostsList.module.css";
 
 function PostList() {
@@ -18,10 +19,12 @@ function PostList() {
 
   return (
     <>
-      <NewPost
-        onBodyChange={bodyChangeHandler}
-        onAuthorChange={authorChangeHandler}
-      />
+      <Modal>
+        <NewPost
+          onBodyChange={bodyChangeHandler}
+          onAuthorChange={authorChangeHandler}
+        />
+      </Modal>
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Mbuso" body="Check out the full course" />
