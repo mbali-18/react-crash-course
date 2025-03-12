679b59bc8494eeb6df16c7f90c856f5f300704d6
mbali-18 <faithfulservent18@gmail.com>
2025-03-12 12:44:20 +0200

    handling side effects with useEffect()

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Post from "./Post";
 import NewPost from "./NewPost";
@@ -8,6 +8,15 @@ import { DiVim } from "react-icons/di";
 
 function PostList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.post);
+    }
+    fetchPosts();
+  }, []);
 
   function addPostHandler(postData) {
     fetch("http://localhost:8080/posts", {
